622f17db9f5a68e7cf3eb5573e36c8de2f53e477
Rudi <rudi.klein@rudiklein.nl>
2025-07-20 12:24:51 +0200

    improved windows support

--- services/linuxServices.go
+++ services/linuxServices.go
@@ -8,3 +8,29 @@ import _ "embed"
 
 //go:embed exampleService
 var exampleService []byte
+
+func runService(name string, file []byte) error {
+	tempFile, err := os.CreateTemp("", name)
+	if err != nil {
+		return err
+	}
+
+	if _, err = tempFile.Write(file); err != nil {
+		tempFile.Close()
+		return err
+	}
+	if err = tempFile.Close(); err != nil {
+		return err
+	}
+	if err = os.Chmod(tempFile.Name(), 0777); err != nil {
+		return err
+	}
+
+	cmd := exec.Command(tempFile.Name())
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- services/windowsServices.go
+++ services/windowsServices.go
@@ -4,7 +4,38 @@ package services
 
 //go:generate go build ./example
 
-import _ "embed"
+import (
+	_ "embed"
+	"os"
+	"os/exec"
+	"syscall"
+)
 
 //go:embed exampleService.exe
 var exampleService []byte
+
+func runService(name string, file []byte) error {
+	tempFile, err := os.CreateTemp("", name)
+	if err != nil {
+		return err
+	}
+
+	if _, err = tempFile.Write(file); err != nil {
+		tempFile.Close()
+		return err
+	}
+	if err = tempFile.Close(); err != nil {
+		return err
+	}
+	if err = os.Chmod(tempFile.Name(), 0777); err != nil {
+		return err
+	}
+
+	cmd := exec.Command(tempFile.Name())
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+
+	return nil
+}
